668e0a0a640c070e308b420f098e9b5a91d9308a
Dinesh Subramanian <itzmedinesh@gmail.com>
2025-04-30 20:07:42 +0530

    Update ApiGatewayConfiguration.java

--- src/main/java/com/microcrafts/gateway/ApiGatewayConfiguration.java
+++ src/main/java/com/microcrafts/gateway/ApiGatewayConfiguration.java
@@ -1,23 +1,15 @@
 package com.microcrafts.gateway;
 
-import org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.gateway.server.mvc.GatewayMvcConfigurer;
+import org.springframework.cloud.gateway.server.mvc.GatewayMvcRouteRegistry;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerResponse;
-
-import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
-import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
-import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.rewritePath;
-import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
-
 
 @Configuration
-public class ApiGatewayConfiguration {
+public class ApiGatewayConfiguration implements GatewayMvcConfigurer {
 
-    @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
+    @Override
+    public void configure(GatewayMvcRouteRegistry registry) {
+        registry
             .route("movieCatalogRoute", r -> r
                 .path("/catalog-api/**")
                 .filters(f -> f.rewritePath("/catalog-api/(?<segment>.*)", "/api/${segment}"))
@@ -27,8 +19,6 @@ public class ApiGatewayConfiguration {
                 .path("/search-api/**")
                 .filters(f -> f.rewritePath("/search-api/(?<segment>.*)", "/api/${segment}"))
                 .uri("http://10.0.0.27:8090")
-            )
-            .build();
+            );
     }
-
 }
