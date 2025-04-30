75b3b2d4ac25bf15b9eb558a93233000e1f40db0
Dinesh Subramanian <itzmedinesh@gmail.com>
2025-04-30 20:14:07 +0530

    Update ApiGatewayConfiguration.java

--- src/main/java/com/microcrafts/gateway/ApiGatewayConfiguration.java
+++ src/main/java/com/microcrafts/gateway/ApiGatewayConfiguration.java
@@ -1,24 +1,26 @@
 package com.microcrafts.gateway;
 
-import org.springframework.cloud.gateway.server.mvc.GatewayMvcConfigurer;
-import org.springframework.cloud.gateway.server.mvc.GatewayMvcRouteRegistry;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.rewritePath;
+import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
 
 @Configuration
-public class ApiGatewayConfiguration implements GatewayMvcConfigurer {
+public class ApiGatewayConfiguration {
 
-    @Override
-    public void configure(GatewayMvcRouteRegistry registry) {
-        registry
-            .route("movieCatalogRoute", r -> r
-                .path("/catalog-api/**")
-                .filters(f -> f.rewritePath("/catalog-api/(?<segment>.*)", "/api/${segment}"))
-                .uri("http://10.0.0.23:8080")
-            )
-            .route("movieSearchRoute", r -> r
-                .path("/search-api/**")
-                .filters(f -> f.rewritePath("/search-api/(?<segment>.*)", "/api/${segment}"))
-                .uri("http://10.0.0.27:8090")
-            );
+    @Bean
+    public RouterFunction<ServerResponse> routeLocator() {
+        return route("movieCatalogRoute")
+                .GET(path("/catalog-api/**"), http("http://10.0.0.23:8080"))
+                .before(rewritePath("/api/v1/(?<segment>.*)", "/api/${segment}"))
+            .route("movieSearchRoute")
+                .GET(path("/search-api/**"), http("http://10.0.0.27:8090"))
+                .before(rewritePath("/books-api/(?<segment>.*)", "/api/${segment}"))
+            .build();
     }
 }
